d1ee4651aba09b898ee9b62f9cce895174fbb837
Vitaliy Matveev <vmatveev@itgroup.lc>
2017-02-16 10:37:29 +0300

    FIX FileField not work blur action if field button focused

--- examples/example.jsx
+++ examples/example.jsx
@@ -1,36 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Form } from '../src'
-
-const App = () => {
-  console.log('App');
-  return (
-    <Form
-      schema={{
-        type: 'object',
-        properties: {
-          date: {
-            type: 'date',
-            title: 'Дата'
-          },
-          date2: {
-            type: 'date',
-            title: 'test'
-          },
-          text: {
-            type: 'string',
-            title: 'Строка'
-          },
-          address: {
-            type: 'address',
-            title: 'Адрес'
-          }
-        },
-        required: ['address']
-      }}
-      value={{date: '11.01.1992'}}
-      onSubmit={data => console.log('submit', data)}
-    />
-  )
+import { Menu, MenuItem } from 'material-ui/Menu'
+class App extends Component {
+  render () {
+    console.log('App');
+    return (
+      <div>
+        <Menu>
+          <MenuItem
+            primaryText='сабмит форты'
+            onClick={ () => this.refs.form.submit() }
+          />
+        </Menu>
+        <Form
+          ref='form'
+          schema={{
+            type: 'object',
+            properties: {
+              date: {
+                type: 'date',
+                title: 'Дата'
+              },
+              date2: {
+                type: 'date',
+                title: 'test'
+              },
+              text: {
+                type: 'string',
+                title: 'Строка'
+              },
+              address: {
+                type: 'address',
+                title: 'Адрес'
+              },
+              file: {
+                type: 'file',
+                title: 'file'
+              },
+              file2: {
+                type: 'file',
+                title: 'file'
+              }
+            },
+            required: ['file', 'file2']
+          }}
+          value={{date: '11.01.1992'}}
+          onSubmit={data => console.log('submit', data)}
+        />
+      </div>
+    )
+  }
 }
 
 export default App
