32335fa2503d6a9616b25c6c5d0d5d221ac25c03
Vitaliy Matveev <vitalmatveev@gmail.com>
2017-02-27 23:58:53 +0300

    [ADD] online editor for Form example

--- examples/example.jsx
+++ examples/example.jsx
@@ -1,31 +1,56 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Form } from '../src'
+import AceEditor from 'react-ace'
+import 'brace/mode/json';
 
-const App = () => {
-  console.log('App');
-  return (
-    <Form
-      schema={{
-        type: 'object',
-        properties: {
-          date: {
-            type: 'date',
-            title: 'Дата'
-          },
-          date2: {
-            type: 'date',
-            title: 'test'
-          },
-          text: {
-            type: 'string',
-            title: 'Строка'
-          }
+import 'brace/theme/monokai';
+class App extends Component {
+  constructor(props) {
+    super (props)
+    var def = {
+      type: 'object',
+      properties: {
+        test: {
+          type: 'string',
+          title: 'Test'
         }
-      }}
-      value={{date: '11.01.1992'}}
-      onSubmit={() => {}}
-    />
-  )
+      }
+    }
+    this.state={
+      editor: JSON.stringify(def),
+      scheme: def
+    }
+  }
+  handleChange = val => this.setState({editor: val})
+  handleSubmit = () => {
+    const scheme = JSON.parse(this.state.editor)
+    this.setState({scheme})
+  }
+  handleDownload = () => {
+    const blob = new Blob([JSON.stringify(JSON.parse(this.state.editor), null, 2)], {type: 'application/json'})
+    let a = document.createElement('a')
+    a.setAttribute('href', URL.createObjectURL(blob, 'application/json'))
+    a.setAttribute('download', 'scheme.json')
+    a.click()
+  }
+  render() {
+    return (
+      <div>
+        <AceEditor
+          mode='json'
+          theme='monokai'
+          name='form_scheme'
+          value={this.state.editor}
+          onChange={this.handleChange}
+        />
+        <button type='button' onClick={this.handleSubmit}>Сгенерировать</button>
+        <button type='button' onClick={this.handleDownload}>Скачать</button>
+        <Form
+          schema={this.state.scheme}
+        />
+      </div>
+    )
+  }
 }
 
 export default App
