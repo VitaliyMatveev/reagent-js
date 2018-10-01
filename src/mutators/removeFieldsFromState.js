3c64e4608d01cfbce8b6c2b1cffe720522f00737
Irina Vivdenko <i.v.vivdenko@cit.rkomi.ru>
2018-10-01 12:05:01 +0300

    Fixed constant updatedState into removeFieldsFromState function considering nesting

--- src/mutators/removeFieldsFromState.js
+++ src/mutators/removeFieldsFromState.js
@@ -1,8 +1,15 @@
+const getField = (obj, path) => {
+  let deepestObj = {...obj};
+  path.split('.').forEach(key => deepestObj = deepestObj[key]);
+  return deepestObj;
+};
+
 export default function removeFieldsFromState([parentFieldName, propList], { formState: { values }, fields}) {
-    const updatedState = parentFieldName ? values[parentFieldName] : values
+    const updatedState = parentFieldName ? getField(values, parentFieldName) : values
+
     propList.map(name => {
-      const removedFieldName = parentFieldName ? `${parentFieldName}.${name}` : name
-      fields[removedFieldName].touched = false
-      delete updatedState[name]
+      const removedFieldName = parentFieldName ? `${parentFieldName}.${name}` : name;
+      fields[removedFieldName].touched = false;
+      delete updatedState[name];
     })
 }
