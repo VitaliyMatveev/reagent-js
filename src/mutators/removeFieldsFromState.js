78fc04841123cc633052d1bba33de90f6a2d0dcd
Irina Vivdenko <i.v.vivdenko@cit.rkomi.ru>
2018-10-03 14:26:08 +0300

    Optimized function for getting the deepest object

--- src/mutators/removeFieldsFromState.js
+++ src/mutators/removeFieldsFromState.js
@@ -1,15 +1,12 @@
-const getField = (obj, path) => {
-  let deepestObj = {...obj};
-  path.split('.').forEach(key => deepestObj = deepestObj[key]);
-  return deepestObj;
-};
+const getDeepestObj = (obj, path) =>
+  path.split('.').reduce((res, key) => res[key], obj);
 
 export default function removeFieldsFromState([parentFieldName, propList], { formState: { values }, fields}) {
-    const updatedState = parentFieldName ? getField(values, parentFieldName) : values
+  const updatedState = parentFieldName ? getDeepestObj(values, parentFieldName) : values;
 
-    propList.map(name => {
-      const removedFieldName = parentFieldName ? `${parentFieldName}.${name}` : name;
-      fields[removedFieldName].touched = false;
-      delete updatedState[name];
-    })
+  propList.map(name => {
+    const removedFieldName = parentFieldName ? `${parentFieldName}.${name}` : name;
+    fields[removedFieldName].touched = false;
+    delete updatedState[name];
+  });
 }
